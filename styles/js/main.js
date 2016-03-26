8eb93bbd99acf529179d147551645cac899e171d
ilearnke <mosessurumen65@gmail.com>
2016-03-26 16:51:09 -0700

    Create main.js

---
+++ styles/js/main.js
@@ -0,0 +1,5 @@
+$('https://ilearnke.github.io/mathematics.html').click(function(e) {
+    e.preventDefault();
+    window.open('https://ilearnke.github.io/mathematics.html');
+    window.open('https://ilearnke.github.io/mathematics.html');
+});
